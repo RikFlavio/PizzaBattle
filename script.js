ff4d197af24e354794f65fb7e7b7e39a948d01b5
RikFlavio <128127396+RikFlavio@users.noreply.github.com>
2023-03-30 11:49:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -133,7 +133,7 @@ function handleReset(button, currentImage, extractedImages) {
     finalMessage.textContent = "Immagini Estratte";
 
     // Imposta nuovamente l'attributo "src" dell'immagine corrente con il percorso dell'immagine della mascotte
-    currentImage.setAttribute("src", "/favicon/pizza.png");
+    currentImage.setAttribute("src", "./favicon/pizza.png");
   }
 }
 
